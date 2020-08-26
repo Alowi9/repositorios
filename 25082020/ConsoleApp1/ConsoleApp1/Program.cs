1d90268dc5e8e55ad6840108ef6ca64e181b2f9b
maxip <maxip@DESKTOP-L6MO8I4>
2020-08-25 22:55:22 -0300

    ejercicioWhile

---
+++ 25082020/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           int numero = 7;
+           int numero2; 
+            Console.WriteLine("Bienvenido!, para ganar adivine el numero");
+            
+            do
+            {
+                
+                string valor2 = Console.ReadLine();
+                numero2 = int.Parse(valor2);
+
+                if(numero != numero2)
+                {
+                    Console.WriteLine("No adivinaste, proba otro numero!");
+                }
+                
+
+            } while (numero != numero2);
+            
+            Console.WriteLine("Ganaste!, BIEN AHI ");
+        }
+    }
+}
